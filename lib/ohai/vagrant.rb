b62bd5aac686b23bf9002d7909acaf226698b3f4
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-11-27 22:09:43 +0200

    Fixed ipaddress not set when primary_nic not set

--- lib/ohai/vagrant.rb
+++ lib/ohai/vagrant.rb
@@ -9,7 +9,7 @@ cloud Mash.new
 if hints
   vagrant.merge!(hints)
   require_plugin "cloud"
-  if vagrant.has_key? :primary_nic
+  if vagrant[:primary_nic]
     ipaddress network[:interfaces][vagrant[:primary_nic]][:addresses].find{|addr, addr_opts| addr_opts[:family] == "inet"}.first
   elsif vagrant[:private_ipv4]
     cloud[:local_ipv4] = vagrant[:private_ipv4]
