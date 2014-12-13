ac6a49a19b2f7979c3065c99569b470ea267d1f1
Carlos Martín <inean.es@gmail.com>
2014-12-13 22:27:31 +0100

    Clean plugin a bit

--- lib/ohai/vagrant.rb
+++ lib/ohai/vagrant.rb
@@ -1,24 +1,36 @@
-Ohai.plugin(:Vboxipaddress) do
+Ohai.plugin(:VboxIpaddress) do
 
-    provides "ipaddress"
-    depends "ipaddress", "network/interfaces", "virtualization/system", "etc/passwd"
+  provides 'ipaddress'
 
-    if File.exist?('/etc/chef/ohai_plugins/vagrant.json')
-        vagrant = JSON.parse(IO.read('/etc/chef/ohai_plugins/vagrant.json'))
+  depends 'ipaddress'
+  depends 'network/interfaces'
+  depends 'virtualization/system'
+  depends 'etc/passwd'
 
-        collect_data(:default) do
-            if virtualization["system"] == "vbox"
-                if etc["passwd"].any? { |k,v| k == "vagrant"}
-                    primary_nic = vagrant["primary_nic"]
-                    if primary_nic && network["interfaces"][primary_nic]
-                        network["interfaces"][primary_nic]["addresses"].each do |ip, params|
-                            if params['family'] == ('inet')
-                                ipaddress ip
-                            end
-                        end
-                    end
-                end
-            end
+  # Ohai hint
+  unless File.exist?('/etc/chef/ohai_plugins/vagrant.json')
+    Chef::Log.fail('Ohai has not set :ipaddress (Missing vagrant.json)')
+    return
+  end
+  vagrant = JSON.parse(IO.read('/etc/chef/ohai_plugins/vagrant.json'))
+
+  # requested nit
+  nic = vagrant['primary_nic']
+  if nic.nil?
+    Chef::Log.debug('nic not set for vagrant-ohai plugin. Skipping')
+    return
+  end
+
+  collect_data(:default) do
+    if etc['passwd'].any? { |k, _v| k == 'vagrant' }
+      if network['interfaces'][nic] && virtualization['system'] == 'vbox'
+        network['interfaces'][nic]['addresses'].each do |ip, params|
+          if params['family'] == ('inet')
+            ipaddress ip
+            Chef::Log.info("Ohai override :ipaddress to #{ip} from #{nic}")
+          end
         end
+      end
     end
+  end
 end
