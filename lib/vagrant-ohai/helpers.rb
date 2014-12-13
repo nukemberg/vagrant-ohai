5790127c9e8ead92c37b7ae0907d65571f77ddbc
Carlos Martín <inean.es@gmail.com>
2014-12-13 21:53:07 +0100

    Add support for new chef provisioners on Vagrant 1.7

--- lib/vagrant-ohai/helpers.rb
+++ lib/vagrant-ohai/helpers.rb
@@ -1,12 +1,12 @@
 module VagrantPlugins
   module Ohai
+    #
     module Helpers
-
       def chef_provisioners
-        @machine.config.vm.provisioners.find_all {|provisioner|
-            [:chef_client, :chef_solo].include? provisioner.name }
+        @machine.config.vm.provisioners.find_all do |provisioner|
+          [:chef_client, :chef_solo, :chef_zero, :chef_apply].include? provisioner.type
+        end
       end
-
     end
   end
 end
