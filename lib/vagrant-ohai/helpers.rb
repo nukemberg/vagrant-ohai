800562de239ebdbb4586650ebc999d7678e1997c
Enrico Stahn <mail@enricostahn.com>
2015-03-23 12:07:15 +1100

    Fix incompatibility with Vagrant 1.7.2
    Fix #6 and #11

--- lib/vagrant-ohai/helpers.rb
+++ lib/vagrant-ohai/helpers.rb
@@ -4,7 +4,8 @@ module VagrantPlugins
     module Helpers
       def chef_provisioners
         @machine.config.vm.provisioners.find_all do |provisioner|
-          [:shell, :chef_client, :chef_solo, :chef_zero, :chef_apply].include? provisioner.name
+          provisioner_name = provisioner.respond_to?('type') ? provisioner.type : provisioner.name
+          [:shell, :chef_client, :chef_solo, :chef_zero, :chef_apply].include? provisioner_name
         end
       end
     end
